f715d468a93d83c3a696d3c681ca16bad0e73536
omprakash kashyap <omprakashkashyap316@gmail.com>
2026-08-06 21:48:55 +0530

    Update lab01q8.c

---
+++ lab01q8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n, i;
+    int *ptr;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    ptr = (int *)malloc(n * sizeof(int));
+    if (ptr == NULL){
+        printf("Memory allocation failed.");
+        return 1;
+    }
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++){
+        scanf("%d", &ptr[i]);
+    }
+    printf("Elements are:\n");
+    for (i = 0; i < n; i++){
+        printf("%d ", ptr[i]);
+    }
+    free(ptr);
+    return 0;
+}
